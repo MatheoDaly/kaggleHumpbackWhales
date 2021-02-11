d6cd82c1adb59a93329cf9e0d79f325813730137
Juventin <53041536+Juventin@users.noreply.github.com>
2021-02-11 18:50:25 +0100

    preprocess input

--- main.py
+++ main.py
@@ -71,8 +71,9 @@ def prepareImages(data, m, dataset, image_size=200):
             # Noir et blanc, et enregistrement
             img = ImageOps.grayscale(img)
 
-            # Enregistrement dans X_train
 
+            # Enregistrement dans X_train
+            img = keras.applications.resnet.preprocess_input(img)
             X_train[count] = img
 
             # Suivi
