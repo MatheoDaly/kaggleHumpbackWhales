760d5925c7d6170f46e8e093151243d960eb3d89
MatheoDalygit config --global user.name MatheoDalygit config --global user.name MatheoDaly <matego.dofmaster@gmail.com>
2021-02-11 09:46:49 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,315 @@
+import os
+
+# import the necessary libraries
+import numpy as np
+import pandas as pd
+import os
+import gc
+import tensorflow as tf
+import random
+from numpy import array, argmax
+from IPython.display import Image
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
+
+from keras.models import Model
+from keras import optimizers
+import cv2
+
+PATH = "../"
+
+# load the training data
+trainData = pd.read_csv(PATH + "/train.csv")
+
+
+def zoom_at(img, zoom, x=100, y=100):
+    w, h = img.size
+    size = (200, 200)
+    img = img.resize((round(w * zoom), round(w * zoom)))
+
+    final_image = Image.new(mode='RGB', size=size, color=(127, 127, 127))
+    final_image.paste(img, (0, 0))
+
+    return final_image
+
+
+def prepareImages(data, m, dataset, image_size=200):
+    print("Preparing images")
+
+    X_train = np.zeros((m, image_size, image_size))
+
+    count = 0
+
+    for fig in data['Image']:
+        if count < m:
+            # load images
+            img = cv2.imread(dataset + "/" + fig)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+            ## FILTRES
+
+            # Flou bilatéral avec d=20, deux passages
+            img = cv2.bilateralFilter(img, 20, 100, 100)
+            img = cv2.bilateralFilter(img, 20, 100, 100)
+            img = Image.fromarray(img)
+            # A partir d'ici on passe d'images cv2 à PIL
+
+            # Resize
+            img = img.resize((200, 200))
+            # Netteté
+            img = img.filter(ImageFilter.SHARPEN)
+            # Embossage
+            img = img.filter(ImageFilter.EMBOSS)
+            # Netteté
+            img = img.filter(ImageFilter.UnsharpMask(radius=2, percent=130, threshold=2))
+
+            ## DATA AUGMENTATION
+
+            # Zoom aléatoire
+            zoom_size = np.random.uniform(0.8, 1.2)
+            img = zoom_at(img, zoom_size)
+
+            # Rotation aléatoire
+            angle = random.randint(-20, 20)
+            img = img.rotate(angle, fillcolor='grey')
+            img = img.filter(ImageFilter.SHARPEN)
+
+            # Noir et blanc, et enregistrement
+            img = ImageOps.grayscale(img)
+
+            # Enregistrement dans X_train
+            X_train[count] = img
+
+            # Suivi
+            if (count % 1000 == 0):
+                print("Processing image: ", count + 1, ", ", fig)
+            count += 1
+
+    count = 0
+
+    print("Finished!")
+
+    return X_train
+
+
+def prepareY(Y):
+    values = array(Y)
+    label_encoder = LabelEncoder()
+    integer_encoded = label_encoder.fit_transform(values)
+
+    onehot_encoder = OneHotEncoder(sparse=False)
+    integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+    onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+
+    y = onehot_encoded
+    return y, label_encoder
+
+
+def prepareY2(Y):
+    values = array(Y)
+    label_encoder = LabelEncoder()
+    integer_encoded = label_encoder.fit_transform(values)
+
+    onehot_encoder = OneHotEncoder(sparse=False)
+    integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+    onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+
+    y = integer_encoded
+    return y, label_encoder
+
+
+# Première initialisation des Y et des X
+Y = trainData['Id']
+y_onehot, label_encoder = prepareY(Y)
+y_integer, label_encoder = prepareY2(Y)
+
+Y_train_onehot = y_onehot
+Y_train_integer = y_integer
+print('init Y ok')
+
+X_train = prepareImages(trainData[:100], 100, PATH + "/train")
+print('init X ok')
+
+
+X_train = np.repeat(X_train[..., np.newaxis], 3, -1)
+
+# BOUCLE
+for i in range(9) :
+    print('---- LOOP '+str(i))
+
+     #Préparation des images
+    X = prepareImages(trainData, 9850,  PATH+"/train")
+    X = np.repeat(X_train[..., np.newaxis], 3, -1)
+     #Concaténation des images
+    X_train = np.concatenate((X_train, X))
+    print('X OK')
+
+     #Concaténation des Y
+    Y_train_onehot = np.concatenate((Y_train_onehot, y_onehot))
+    Y_train_integer = np.concatenate((Y_train_integer, y_integer))
+    print('Y OK')
+
+
+baseModel = tf.keras.applications.ResNet50(weights="imagenet", include_top=False, input_tensor=Input(shape=(200, 200, 3)))
+for layer in baseModel.layers:
+	layer.trainable = False
+mod = baseModel.output
+
+mod = BatchNormalization(axis = 3, name = 'bn0')(mod)
+mod = Activation('relu')(mod)
+
+mod = MaxPooling2D((2, 2), name='max_pool')(mod)
+mod = Conv2D(64, (2, 2), strides = (1,1), name="conv1")(mod)
+mod = Activation('relu')(mod)
+mod = AveragePooling2D((1, 1), name='avg_pool')(mod)
+
+mod = Flatten()(mod)
+mod = Dense(500, activation="relu", name='rl')(mod)
+mod = Dropout(0.8)(mod)
+mod = Dense(4251, activation='softmax', name='sm')(mod)
+
+mod = Model(inputs=baseModel.input, outputs=mod)
+opt = optimizers.Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+
+mod.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+history = mod.fit(X_train, Y_train_onehot[:100], epochs=100, batch_size=100, verbose=1)
+
+
+#open test data
+test = os.listdir(PATH+"/test/")
+print(len(test))
+
+#separate data into different DataFrames due to memory constraints
+col = ['Image']
+testData1 = pd.DataFrame(test[0:3899], columns=col)
+testData2 = pd.DataFrame(test[3900:7799], columns=col)
+testData3 = pd.DataFrame(test[7800:11699], columns=col)
+testData4 = pd.DataFrame(test[11700:15609], columns=col)
+testData = pd.DataFrame(test, columns=col)
+
+#X_test = prepareImages(testData1, 15610, "test")
+gc.collect()
+X = prepareImages(testData1, 3900, PATH+"/test")
+X /= 255
+
+X = np.repeat(X[..., np.newaxis], 3, -1)
+predictions1 = mod.predict(np.array(X), verbose=1)
+gc.collect()
+
+X = prepareImages(testData2, 3900, PATH+"/test")
+X /= 255
+X = np.repeat(X[..., np.newaxis], 3, -1)
+predictions2 = mod.predict(np.array(X), verbose=1)
+gc.collect()
+X = prepareImages(testData3, 3900,  PATH+"/test")
+X /= 255
+X = np.repeat(X[..., np.newaxis], 3, -1)
+predictions3 = mod.predict(np.array(X), verbose=1)
+gc.collect()
+X = prepareImages(testData4, 3910, PATH+"/test")
+X /= 255
+X = np.repeat(X[..., np.newaxis], 3, -1)
+predictions4 = mod.predict(np.array(X), verbose=1)
+gc.collect()
+
+#concatenate all the predictions in the same vector
+predictions = np.concatenate((predictions1, predictions2), axis=0)
+predictions = np.concatenate((predictions, predictions3), axis=0)
+predictions = np.concatenate((predictions, predictions4), axis=0)
+gc.collect()
+
+
+#choose predictions with highest probability. For each value I choose, I set the probability to zero, so it can't be picked again.
+
+
+copy_pred = np.copy(predictions)
+idx = np.argmax(copy_pred, axis=1)
+copy_pred[:,idx] = 0
+idx2 = np.argmax(copy_pred, axis=1)
+copy_pred[:, idx2] = 0
+idx3 = np.argmax(copy_pred, axis=1)
+copy_pred[:, idx3] = 0
+idx4 = np.argmax(copy_pred, axis=1)
+copy_pred[:, idx4] = 0
+idx5 = np.argmax(copy_pred, axis=1)
+
+#convert the one-hot vectors to their names
+results = []
+
+print(idx[0:10])
+print(idx2[0:10])
+print(idx3[0:10])
+print(idx4[0:10])
+print(idx5[0:10])
+threshold = 0.05 #threshold - only consider answers with a probability higher than it
+for i in range(0, predictions.shape[0]):
+#for i in range(0, 10):
+    each = np.zeros((4251, 1))
+    each2 = np.zeros((4251, 1))
+    each3 = np.zeros((4251, 1))
+    each4 = np.zeros((4251, 1))
+    each5 = np.zeros((4251, 1))
+    if((predictions[i, idx5[i]] > threshold)):
+        each5[idx5[i]] = 1
+        each4[idx4[i]] = 1
+        each3[idx3[i]] = 1
+        each2[idx2[i]] = 1
+        each[idx[i]] = 1
+        tags = [label_encoder.inverse_transform([argmax(each)])[0], label_encoder.inverse_transform([argmax(each2)])[0], label_encoder.inverse_transform([argmax(each3)])[0], label_encoder.inverse_transform([argmax(each4)])[0], label_encoder.inverse_transform([argmax(each5)])[0]]
+    else:
+        if((predictions[i, idx4[i]] > threshold)):
+            print(predictions[i, idx4[i]])
+            each4[idx4[i]] = 1
+            each3[idx3[i]] = 1
+            each2[idx2[i]] = 1
+            each[idx[i]] = 1
+            tags = [label_encoder.inverse_transform([argmax(each)])[0], label_encoder.inverse_transform([argmax(each2)])[0], label_encoder.inverse_transform([argmax(each3)])[0], label_encoder.inverse_transform([argmax(each4)])[0]]
+        else:
+            if((predictions[i, idx3[i]] > threshold)):
+                each3[idx3[i]] = 1
+                each2[idx2[i]] = 1
+                each[idx[i]] = 1
+                tags = [label_encoder.inverse_transform([argmax(each)])[0], label_encoder.inverse_transform([argmax(each2)])[0], label_encoder.inverse_transform([argmax(each3)])[0]]
+            else:
+                if((predictions[i, idx2[i]] > threshold)):
+                    each2[idx2[i]] = 1
+                    each[idx[i]] = 1
+                    tags = [label_encoder.inverse_transform([argmax(each)])[0], label_encoder.inverse_transform([argmax(each2)])[0]]
+                else:
+                    each[idx[i]] = 1
+                    tags = label_encoder.inverse_transform([argmax(each)])[0]
+    results.append(tags)
+
+# write the predictions in a file to be submitted in the competition.
+myfile = open('output.csv', 'w')
+
+column = ['Image', 'Id']
+
+wrtr = csv.writer(myfile, delimiter=',')
+wrtr.writerow(column)
+
+for i in range(0, testData.shape[0]):
+    pred = ""
+    if (len(results[i]) == 5):
+        if (results[i][4] != results[i][0]):
+            pred = results[i][0] + " " + results[i][1] + " " + results[i][2] + " " + results[i][3] + " " + results[i][4]
+        else:
+            pred = results[i][0] + " " + results[i][1] + " " + results[i][2] + " " + results[i][3]
+    else:
+        if (len(results[i]) == 4):
+            pred = results[i][0] + " " + results[i][1] + " " + results[i][2] + " " + results[i][3]
+        else:
+            if (len(results[i]) == 3):
+                pred = results[i][0] + " " + results[i][1] + " " + results[i][2]
+            else:
+                if (len(results[i]) == 2):
+                    pred = results[i][0] + " " + results[i][1]
+                else:
+                    pred = results[i]
+
+    result = [testData['Image'][i], pred]
+    # print(result)
+    wrtr.writerow(result)
+
+myfile.close()
